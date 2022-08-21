077a88210b7d12e5f5bfa85cd48b0adfdfd7d7b8
jfcava <juancavallieri@gmail.com>
2022-08-21 18:39:11 -0300

    Comandos GIT y GITHUB

---
+++ comandosGIT.js
@@ -0,0 +1,31 @@
+// 1- TRAER REPOSITORIO DE GITHUB
+
+// git clone "https://github.com/jfcava/comandosGIT.git"
+
+
+
+// 2- VER ESTADO DEL REPOSITORIO
+
+// git status
+
+
+
+// 3- AGREGAR TODOS LOS ARCHIVOS, O CAMBIOS AL REPOSITORIO
+
+// git add .
+
+// SI QUIERO ESPECIFICAR EL ARCHIVO 
+
+// git add ./Curso1/comandosGIT.js
+
+
+
+// 4- HACER UN COMMIT DEL REPOSITORIO
+
+// git commit -m "mensaje_que_quieras_poner"
+
+
+
+// 5- MANDAR A GITHUB
+
+// git push
